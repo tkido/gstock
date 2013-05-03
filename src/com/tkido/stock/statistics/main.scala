5b0cd9a7f15bda8b918069afdc52d527f66321a7
Takanori Kido <takanorikido@gmail.com>
2013-05-03 15:48:44 +0900

    tkdio.

--- src/com/tkido/stock/statistics/main.scala
+++ src/com/tkido/stock/statistics/main.scala
@@ -1,59 +1,34 @@
 package com.tkido.stock.statistics
 
 import scala.collection.mutable.{Map => MMap}
-import scala.collection.mutable.{Set => MSet}
 
 object main extends App {
-  case class Stock(id:String, name:String, value:String, delta:String, group:String)  
   case class Deal(id:String, name:String, price:Int)
 
   object DealType extends Enumeration {
     val NEUTRAL, BUY, SELL = Value
   }
 
-  val (stocksmap, stocks) = makeStocks()
   val (namesmap, deals) = parseDeals()
   
   for(id <- namesmap.keys){
-    val value = if (stocksmap.contains(id))
-      stocksmap(id).value.filter(_ != ',').toInt
-      else 0
-    val price = sumDeals(id) + value
-    println(id + "\t" + namesmap(id) + "\t" + price)
+    println(id + "\t" + namesmap(id) + "\t" + sumDeals(id))
   }
   
   def sumDeals(target:String): Int = {
     val targets = deals.filter(_.id == target)
-    val sum = targets.foldLeft(0)(_+_.price)
-    sum
-  }
-
-  def makeStocks() :(MMap[String, Stock], MSet[Stock]) = {
-    import scala.io.Source
-    val s = Source.fromFile("data/statistics/jp.txt")
-    val lines = try s.getLines.toList finally s.close
-    
-    val stocks = MSet[Stock]()
-    val stocksmap = MMap[String, Stock]()
-    for(line <- lines){
-      val array = line.split("\t")
-      val idname = array(1).split(" ")
-      val id = idname(0)
-      val stock = Stock(id, idname(1), array(9), array(7), "")
-      
-      stocks += stock
-      stocksmap(id) = stock
-    }
-    (stocksmap, stocks)
+    targets.foldLeft(0)(_+_.price)
   }
   
-  def parseDeals() :(MMap[String, String], MSet[Deal]) = {
+  def parseDeals() :(MMap[String, String], List[Deal]) = {
     import scala.io.Source
+    import DealType._ 
+    
     val s = Source.fromFile("data/statistics/statistics.txt")
     val lines = try s.getLines.toList finally s.close
     
     val namesmap = MMap[String, String]()
-    val deals = MSet[Deal]()
+    var deals = List[Deal]()
     for(line <- lines.tail){
       val array = line.split("\t")
       val name = array(1)
@@ -62,19 +37,17 @@ object main extends App {
       namesmap(id) = name
       
       val dealType = array(4) match {
-        case "Š”Ž®Œ»•¨”ƒ" => DealType.BUY
-        case "Š”Ž®Œ»•¨”„" => DealType.SELL
-        case "”z“–"       => DealType.SELL
-        case _            => DealType.NEUTRAL
+        case "Š”Ž®Œ»•¨”ƒ" => BUY
+        case "Š”Ž®Œ»•¨”„" => SELL
+        case "”z“–"       => SELL
+        case _            => NEUTRAL
       }
-      
-      val coefficient = dealType match {
-        case DealType.BUY     => -1
-        case DealType.SELL    => +1
-        case DealType.NEUTRAL => 0
+      val price = dealType match {
+        case BUY     => -1 * array(13).toInt
+        case SELL    => +1 * array(13).toInt
+        case NEUTRAL => 0
       }
-      val price = coefficient * array(13).toInt
-      deals += Deal(id, name, price)
+      deals = Deal(id, name, price) :: deals
     }
     (namesmap, deals)
   }
