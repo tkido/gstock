07f76169b5576f689f739bd5332fc65cc28351d4
Takanori Kido <takanorikido@gmail.com>
2013-06-18 15:53:38 +0900

    tkido.

--- src/com/tkido/stock/xbrl/main.scala
+++ src/com/tkido/stock/xbrl/main.scala
@@ -1,5 +1,6 @@
 package com.tkido.stock.xbrl
 
 object main extends App {
-	
+  println("xbrl start")
+  XbrlParser.parse("data/xbrl/3085/XBRL_20130617_105616/S000D3P0/jpfr-asr-E03513-000-2012-12-31-01-2013-03-25.xbrl")
 }
