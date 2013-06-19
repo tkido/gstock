0e1519d0047d3a5a466964845df66f4d1415187a
Takanori Kido <takanorikido@gmail.com>
2013-06-19 09:16:18 +0900

    tkido.

--- src/com/tkido/stock/xbrl/main.scala
+++ src/com/tkido/stock/xbrl/main.scala
@@ -1,6 +1,8 @@
 package com.tkido.stock.xbrl
 
 object main extends App {
-  println("xbrl start")
-  XbrlParser.parse("data/xbrl/3085/XBRL_20130617_105616/S000D3P0/jpfr-asr-E03513-000-2012-12-31-01-2013-03-25.xbrl")
+  println("start")
+  val codes = List("3085")
+  val companies = codes.map(Company(_))
+  for(company <- companies) println(company)
 }
