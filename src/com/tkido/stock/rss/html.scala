9d9f605c77e316245b49737a0c3ec05bfa8fcde3
Takanori Kido <takanorikido@gmail.com>
2013-07-13 17:23:59 +0900

    tkdio.

---
+++ src/com/tkido/stock/rss/html.scala
@@ -0,0 +1,43 @@
+package com.tkido.stock.rss
+
+class Html(url:String, charset:String) {
+  import scala.io.Source
+  import scala.util.matching.Regex
+  
+  val lines = Source.fromURL(url, charset).getLines
+  
+  def getPreviousLineOf(rgex:Regex) :String = {
+    var last = ""
+    var target = ""
+    for(line <- lines){
+      if(rgex.findFirstIn(line).isDefined)
+        target = last
+      last = line
+    }
+    Html.removeTags(target)
+  }
+  
+  def getNextLineOf(rgex:Regex) :String = {
+    var flag = false
+    var target = ""
+    
+    for(line <- lines){
+      if(flag){
+        target = line
+        flag = false
+      }
+      if(rgex.findFirstIn(line).isDefined)
+        flag = true
+    }
+    Html.removeTags(target)
+  }  
+}
+
+object Html{
+  def apply(url:String) = new Html(url, "UTF-8")
+  def apply(url:String, charset:String) = new Html(url, charset)
+  
+  def removeTags(string:String) :String =
+    string.replaceAll("""<.*?>""", "").trim
+
+}
