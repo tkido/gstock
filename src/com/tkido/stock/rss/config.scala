93cf8b6b6234f1798c9b98b73805320bced6da5c
Takanori Kido <takanorikido@gmail.com>
2013-07-17 12:58:38 +0900

    ktido.

--- src/com/tkido/stock/rss/config.scala
+++ src/com/tkido/stock/rss/config.scala
@@ -3,12 +3,11 @@ package com.tkido.stock.rss
 object Config {
   val prop = new java.util.Properties()
   prop.load(new java.io.FileInputStream("data/rss/conf.properties"))
+
+  val offset = get("offset").toInt
   
   def get(key:String) :String =
     prop.getProperty(key)
-  
-  def offset :Int =
-    prop.getProperty("offset").toInt
   
   
 }
