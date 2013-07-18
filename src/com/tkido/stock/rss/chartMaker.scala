2492888f3e6f7c1889e4825b51ece73651702329
Takanori Kido <takanorikido@gmail.com>
2013-07-18 12:30:14 +0900

    tkdi.o

--- src/com/tkido/stock/rss/chartMaker.scala
+++ src/com/tkido/stock/rss/chartMaker.scala
@@ -1,54 +1,8 @@
 package com.tkido.stock.rss
 
 object ChartMaker {
-
-  private val templete = """
-<html>
-  <head>
-    <meta charset="shift-jis" />
-    <title>%s</title>
-    
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-
-      // Load the Visualization API and the piechart package.
-      google.load('visualization', '1.0', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Tpic');
-        data.addColumn('number', 'Slices');
-        data.addRows([%s]);
-
-        // Set chart options
-        var options = {'width':800,
-                       'height':400};
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>
-  </head>
-
-  <body>
-    <h1>%s</h1>
-    <p>%s%s</p>
-    <!--Div that will hold the pie chart-->
-    <div id="chart_div"></div>
-  </body>
-</html>
-"""
-    
+  private val templete = TextFile.read("data/rss/template.html")
+  
   def make(company:Company){
     val code = company.data("ID")
     val name = company.data("名称")
--- src/com/tkido/stock/rss/textFile.scala
+++ src/com/tkido/stock/rss/textFile.scala
@@ -3,7 +3,7 @@ package com.tkido.stock.rss
 object TextFile {
   import scala.io.Source
   import java.io.PrintWriter
-  
+
   def readLines(path:String, charset:String) :List[String] = {
     val s = Source.fromFile(path, charset)
     val lines = try s.getLines.toList finally s.close
@@ -11,7 +11,12 @@ object TextFile {
   }
   def readLines(path:String) :List[String] =
     readLines(path, "UTF-8")
-  
+
+  def read(path:String, charset:String) :String =
+    readLines(path, charset).mkString("\n")
+  def read(path:String) :String =
+    read(path, "UTF-8")
+
   def writeString(path:String, data:String) {
     val out = new PrintWriter(path)
     out.println(data)
