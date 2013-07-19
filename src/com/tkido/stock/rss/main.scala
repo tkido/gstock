b2379b447be3ad3dcad3401383c5d7cdea767f72
Takanori Kido <takanorikido@gmail.com>
2013-07-19 16:00:46 +0900

    tkdio.

--- src/com/tkido/stock/rss/main.scala
+++ src/com/tkido/stock/rss/main.scala
@@ -1,12 +1,20 @@
 package com.tkido.stock.rss
 
 object main extends App {
-  val codes = TextFile.readLines("data/rss/table.txt")
-  val companies = codes.map(Company(_))
-  for(company <- companies) ChartMaker.make(company)
-  val strings =
-    for((company, index) <- companies.zipWithIndex)
-      yield company.toStringForExcel(Config.offset + index)
-  TextFile.writeString("data/rss/result.txt", strings.mkString("\n"))
-  println("OK!!")
+  def main(){
+    val codes = TextFile.readLines("data/rss/table.txt")
+    val companies = codes.map(Company(_))
+    for(company <- companies) ChartMaker.make(company)
+    val strings =
+      for((company, index) <- companies.zipWithIndex)
+        yield company.toStringForExcel(Config.offset + index)
+    TextFile.writeString("data/rss/result.txt", strings.mkString("\n"))
+  }
+  
+  try{
+    Logger.level = Logger.FATAL
+    main()
+  }finally{
+    Logger.close()
+  }
 }
