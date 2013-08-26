1b192360815efdb6f7b09702b6a9c43837b9c343
tkido <takanorikido@gmail.com>
2013-08-26 18:52:56 +0900

    add textFile.

---
+++ src/com/tkido/tools/textFile.scala
@@ -0,0 +1,26 @@
+package com.tkido.tools
+
+object TextFile {
+  import scala.io.Source
+  import java.io.PrintWriter
+
+  def readLines(path:String, charset:String) :List[String] = {
+    val s = Source.fromFile(path, charset)
+    val lines = try s.getLines.toList finally s.close
+    lines.map(_.stripLineEnd)
+  }
+  def readLines(path:String) :List[String] =
+    readLines(path, "UTF-8")
+
+  def read(path:String, charset:String) :String =
+    readLines(path, charset).mkString("\n")
+  def read(path:String) :String =
+    read(path, "UTF-8")
+
+  def write(path:String, data:String) {
+    val out = new PrintWriter(path)
+    out.println(data)
+    out.close
+  }  
+  
+}
