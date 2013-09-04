cc57b4ff7010bac362d601eb147e6f14fecef07b
tkido <takanorikido@gmail.com>
2013-09-04 20:00:17 +0900

    add mail script.

--- script/shell/rss_mail.sh
+++ script/shell/rss_mail.sh
@@ -1,1 +1,8 @@
-/usr/sbin/sendmail takanorikido@gmail.com < data/rss/mail.txt
+/usr/sbin/sendmail takanorikido@gmail.com <<_EOT_
+From: jenkins@tkido.com
+To: takanorikido@gmail.com
+Subject: ${BUILD_TAG} completed!!
+
+Download URL:
+http://tkido.com/stock/result.txt
+_EOT_
