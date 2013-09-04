a8eccbbe99bdce77861c9d3bff3c0220384a1282
tkido <takanorikido@gmail.com>
2013-09-04 20:04:46 +0900

    fix mail.

--- script/shell/rss_mail.sh
+++ script/shell/rss_mail.sh
@@ -1,7 +1,7 @@
 /usr/sbin/sendmail takanorikido@gmail.com <<_EOT_
 From: jenkins@tkido.com
 To: takanorikido@gmail.com
-Subject: ${BUILD_TAG} completed!!
+Subject: ${JOB_NAME} ${BUILD_NUMBER} completed!!
 
 Download URL:
 http://tkido.com/stock/result.txt
