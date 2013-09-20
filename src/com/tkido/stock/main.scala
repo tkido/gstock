4f89d0d1592f0700d0e1529a04bbebadab6fe3d3
tkido <takanorikido@gmail.com>
2013-09-20 20:41:41 +0900

    add ChartMakerJp

--- src/com/tkido/stock/main.scala
+++ src/com/tkido/stock/main.scala
@@ -3,7 +3,6 @@ package com.tkido.stock
 object main extends App {
   import com.tkido.stock.rss.ChartMaker
   import com.tkido.stock.rss.Company
-  import com.tkido.stock.rss.CompanyJp
   import com.tkido.stock.ufo.XbrlDownloader
   import com.tkido.tools.Text
   import com.tkido.tools.Logger
@@ -12,16 +11,15 @@ object main extends App {
   
   val codes = Text.readLines("data/table.txt")
   
-  for(code <- codes) { XbrlDownloader.download(code) }
+  for(code <- codes) XbrlDownloader.download(code)
   
   val range = Range(Config.offset, Config.offset + codes.size)
-  val pairs = codes zip range
-  val companies = pairs.par.map(pair => Company(pair._1, pair._2))
+  val companies = (codes zip range).par.map(p => Company(p._1, p._2))
   
   val strings = companies.map(_.toString)
   Text.write("data/result.txt", strings.mkString("\n"))
   
-  companies.collect{ case company:CompanyJp => ChartMaker.make(company) }
+  for(company <- companies) ChartMaker(company)
   
   Logger.close()
 }
--- src/com/tkido/stock/rss/chartMaker.scala
+++ src/com/tkido/stock/rss/chartMaker.scala
@@ -1,60 +1,10 @@
 package com.tkido.stock.rss
 
 object ChartMaker {
-  import com.tkido.tools.Text
-  
-  private val templete = Text.read("data/rss/template.html")
-  
-  def make(company:Company){
-    val data = company.data
-    
-    val code     = data.getOrElse("ID", "")
-    val business = data.getOrElse("事業", "")
-    val name     = data.getOrElse("名称", "")
-    val feature  = data.getOrElse("特色", "")
-    val table    = data.getOrElse("表", "")
-    
-    val reDate = """\(\d{4}\.\d{1,2}\)"""
-    val reHeader = "【.*?】"
-    val reOther = "【.*"
-    val reData = """(.*?)(\d+)(\(\d+\))?""".r
-    
-    def getDate() :String =
-      reDate.r.findFirstMatchIn(business) match {
-        case Some(m) => m.group(0)
-        case None    => ""
-      }
-    def getHeader() :String =
-      reHeader.r.findFirstMatchIn(business) match {
-        case Some(m) => m.group(0)
-        case None    => ""
-      }
-    def getOther() :String =
-      reOther.r.findFirstMatchIn(business.replaceFirst(reHeader, "")) match {
-        case Some(m) => m.group(0).replaceFirst(reDate, "")
-        case None    => ""
-      }
-    
-    def getRows() :String = {
-      val str = business.replaceFirst(reDate, "").replaceFirst(reHeader, "").replaceFirst(reOther, "")
-      val rows = str.split('、')
-      
-      def stringToPairs(raw: String): Pair[String, String] = {
-        reData.findFirstMatchIn(raw) match {
-          case None    => "" -> ""
-          case Some(m) => {
-            val g3 = if(m.group(3) == null) "" else m.group(3)
-            Pair(m.group(1)+g3, m.group(2))
-          }
-        }
-      }
-      val pairs = rows.map(stringToPairs)
-      pairs.map(x => """['%s', %s]""".format(x._1, x._2) ).mkString(",\n")
+  def apply(company:Company){
+    company match {
+      case c:CompanyJp => ChartMakerJp(c)
+      case _           => ()
     }
-    
-    val title = code + " " + name + getHeader + getDate
-    
-    val html = templete.format(title, getRows, title, feature, getOther, table)
-    Text.write("data/rss/%s.html".format(code), html)
   }
 }
