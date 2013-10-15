3e937e4b73fbb2339e772e33751722d8d0bbd965
tkido <takanorikido@gmail.com>
2013-10-15 17:51:49 +0900

    add FileLogger and PrintLogger http://tkido.com/redmine/issues/3788

--- src/com/tkido/tools/logger.scala
+++ src/com/tkido/tools/logger.scala
@@ -1,8 +1,7 @@
 package com.tkido.tools
 
 object Logger{
-  import java.io.FileOutputStream
-  import java.io.OutputStreamWriter
+  import java.io.File
   import java.util.Date
   
   val DEBUG = 1
@@ -15,27 +14,24 @@ object Logger{
   
   private val template = "%tY_%<tm%<td_%<tH%<tM_%<tS"
   
-  val started = template.format(new Date)
-  private val fileName = "log/%s.log".format(started)
+  private val started = template.format(new Date)
   
-  private val encode = "utf-8"
-  private val append = true
-  private val stream = new FileOutputStream(fileName, append)
-  private val writer = new OutputStreamWriter(stream, encode)
+  val logger =
+    if(new File("log/").exists) new FileLogger
+    else                        new PrintLogger
   
   fatal("STARTED at %s".format(started))
-  println("STARTED at %s".format(started))
   
   def close() {
     val ended = template.format(new Date)
     fatal("ENDED at %s".format(ended))
-    println("ENDED at %s".format(ended))
-    writer.close()
+    logger.close()
   }
   
   def log(arg: Any) {
-    writer.write(arg.toString + '\n')
+    logger.log(arg)
   }
+  
   def debug(arg: => Any) { if (level <= DEBUG) log(arg) }
   def info (arg: => Any) { if (level <= INFO ) log(arg) }
   def warn (arg: => Any) { if (level <= WARN ) log(arg) }
@@ -48,4 +44,40 @@ object Logger{
   def isError = (level <= ERROR)
   def isFatal = (level <= FATAL)
   
+}
+
+abstract class Logger{
+  def log(arg: Any)
+  def close()
+}
+
+class FileLogger extends Logger{
+  import java.io.FileOutputStream
+  import java.io.OutputStreamWriter
+  import java.util.Date
+  
+  private val template = "%tY_%<tm%<td_%<tH%<tM_%<tS"
+  private val started = template.format(new Date)
+  private val fileName = "log/%s.log".format(started)
+  
+  private val encode = "utf-8"
+  private val append = true
+  private val stream = new FileOutputStream(fileName, append)
+  private val writer = new OutputStreamWriter(stream, encode)
+  
+  def log(arg: Any) {
+    writer.write(arg.toString + '\n')
+  }
+  
+  def close() {
+    writer.close()
+  }
+}
+
+class PrintLogger extends Logger{
+  def log(arg: Any) {
+    println(arg)
+  }
+  
+  def close() {}
 }
