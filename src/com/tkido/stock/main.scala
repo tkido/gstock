705953bdc96bc1e6022c448b2a838dab5a330b90
tkido <takanorikido@gmail.com>
2013-10-20 23:06:43 +0900

    add Processor http://tkido.com/redmine/issues/3794

--- src/com/tkido/stock/main.scala
+++ src/com/tkido/stock/main.scala
@@ -1,32 +1,22 @@
 package com.tkido.stock
 
 object main extends App {
-  import com.tkido.stock.edinet
-  import com.tkido.stock.rss.ChartMaker
-  import com.tkido.stock.rss.Company
-  import com.tkido.stock.tdnet
   import com.tkido.tools.Logger
   import com.tkido.tools.Text
   
   Logger.level = Config.logLevel
   
   val codes = Text.readLines("data/table.txt")
-  
-  codes.map(edinet.XbrlDownloader(_))
-  codes.map(tdnet.XbrlDownloader(_))
-  
   val range = Range(Config.offset, Config.offset + codes.size)
   val pairs = codes zip range
   
-  val companies =
+  val data =
     if(Config.parFlag)
-      pairs.par.map(p => Company(p._1, p._2))
+      pairs.par.map(p => Processor(p))
     else
-      pairs.map(p => Company(p._1, p._2))
+      pairs.map(p => Processor(p))
   
-  Text.write("data/result.txt", companies.mkString("\n"))
-  
-  companies.map(ChartMaker(_))
+  Text.write("data/result.txt", data.mkString("\n"))
   
   Logger.close()
 }
