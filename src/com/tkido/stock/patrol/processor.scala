d1d0456704b4c496713fd8e859582fddadaca523
tkido <takanorikido@gmail.com>
2014-02-11 18:03:19 +0900

    add patrol/company.scala

--- src/com/tkido/stock/patrol/processor.scala
+++ src/com/tkido/stock/patrol/processor.scala
@@ -2,14 +2,13 @@ package com.tkido.stock.patrol
 
 object Processor {
   import com.tkido.stock.edinet
-  import com.tkido.stock.rss.CompanyJpOther
   import com.tkido.stock.tdnet
   
   def apply(code:String) :String = {
     edinet.XbrlDownloader(code)
     tdnet.XbrlDownloader(code)
     
-    val company = CompanyJpOther(code, 0)
+    val company = Company(code)
     company.toString
   }
   
