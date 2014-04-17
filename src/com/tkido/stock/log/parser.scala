a46cca6213e2c24805d89170f974126fbe48afb4
tkido <takanorikido@gmail.com>
2014-04-17 18:58:11 +0900

    display log on chart

--- src/com/tkido/stock/log/parser.scala
+++ src/com/tkido/stock/log/parser.scala
@@ -10,7 +10,7 @@ object Parser {
     
     def convert(src:List[Array[String]]) :List[List[String]] = {
       def subconvert(arr:Array[String]) :List[String] = {
-        val (date, trade, volume, price, amount) = (arr(0), arr(4), arr(5), arr(6), arr(10))
+        val (date, trade, volume, price, amount) = (arr(0), arr(4).replaceFirst("株式", ""), arr(5), arr(6), arr(10))
         List(date, trade, volume, price, amount)
       }
       var rst = src.map(subconvert(_))
