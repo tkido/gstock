7361284d75ede0b787abac9c75e86b314691e940
tkido <takanorikido@gmail.com>
2015-10-04 04:49:35 +0900

    Refactoring

--- src/com/tkido/stock/page/pageMakerJp.scala
+++ src/com/tkido/stock/page/pageMakerJp.scala
@@ -3,48 +3,22 @@ package com.tkido.stock.page
 import com.tkido.tools.Log
 import com.tkido.tools.Tengine
 import com.tkido.tools.Text
+import com.tkido.tools.Regx.{matchedAndRest, collectMatched}
 import java.util.Date
-
-import scala.util.matching.Regex
 
 object PageMakerJp {
   private val tEngine = Tengine("data/rss/templateJP.html")
   private val tData = Tengine("data/rss/templateData.html")
   private val tDiv = Tengine("data/rss/templateDiv.html")
-  
-  def matchedAndRest(regp:String, str:String) :(String, String) = {
-    val regx = regp.mkString("^.*?(", "", ").*$").r
-    str match{
-      case regx(s) => (s, str.replaceFirst(Regex.quote(s), ""))
-      case _ => ("", str)
-    }
-  }
-  
-  def collectMatched(regp:String, str:String) :List[String] = {
-    def go(regp:String, str:String, list:List[String]) :List[String] = {
-      val regx = regp.mkString("^.*?(", "", ").*$").r
-      str match{
-        case regx(s) => s :: go(regp, str.replaceFirst(Regex.quote(s), ""), list)
-        case _ => list
-      }
-    }
-    go(regp, str, List())
-  }
-  
+
   def apply(data:Map[String, String]){
-    Log f "PageMakerJp"
     val business = data.getOrElse("事業", "")
-    Log f business
     
     val (date, str1) = matchedAndRest("""\(\d{4}\.\d{1,2}\)""", business)
-    Log f date
-    Log f str1
     
     case class Sector(header:String, rows:String)
     def toSector(str:String) :Sector = {
       val (header, str2) = matchedAndRest("""【.*?】""", str)
-      Log f header
-      Log f str2
 
       val reData = """(.*?)(\d+)(\(-?\d+\))?""".r
       val rows = {
@@ -62,13 +36,10 @@ object PageMakerJp {
           .map(p => s"""['${p._1}', ${p._2}]""")
           .mkString(",\n")
       }
-      Log f rows
       Sector(header, rows)
     }
     
-    val sectors = collectMatched("""【.*?】[^【]*""", str1).map(toSector(_))
-    Log f sectors
-    
+    val sectors = collectMatched("""【.*?】[^【]*""", str1).map(toSector(_))    
     val secnums = (sectors zip Range(0, sectors.size))
     
     val today = "%tY_%<tm%<td".format(new Date)
@@ -80,7 +51,6 @@ object PageMakerJp {
     val jss = secnums.map{case (sec, i) =>
       tData(data ++ Map("today" -> today, "num" -> i.toString, "rows" -> sec.rows))
     }.mkString("\n")
-   
     
     val added = Map("date" -> date,
                      "divs" -> divs,
