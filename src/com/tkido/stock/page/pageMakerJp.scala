a50360eec9deddc15417b1d9ca77ee372153b30b
tkido <takanorikido@gmail.com>
2015-10-04 03:31:44 +0900

    Refactoring

--- src/com/tkido/stock/page/pageMakerJp.scala
+++ src/com/tkido/stock/page/pageMakerJp.scala
@@ -1,20 +1,90 @@
 package com.tkido.stock.page
 
+import com.tkido.tools.Log
 import com.tkido.tools.Tengine
 import com.tkido.tools.Text
 import java.util.Date
 
+import scala.util.matching.Regex
+
 object PageMakerJp {
   private val tEngine = Tengine("data/rss/templateJP.html")
   
+  def matchedAndRest(regp:String, str:String) :(String, String) = {
+    val regx = regp.r
+    str match{
+      case regx(s) => (s, str.replaceFirst(Regex.quote(s), ""))
+      case _ => ("", str)
+    }
+  }
+  
+  def allMatched(regp:String, str:String) :List[String] = {
+    def go(regp:String, str:String, list:List[String]) :List[String] = {
+      val regx = regp.r
+      str match{
+        case regx(s) => s :: go(regp, str.replaceFirst(Regex.quote(s), ""), list)
+        case _ => list
+      }
+    }
+    go(regp, str, List())
+  }
+  
   def apply(data:Map[String, String]){
+    Log f "PageMakerJp"
+    val business = data.getOrElse("事業", "")
+    Log f business
+    
+    val (date, str1) = matchedAndRest(""".*?(\(\d{4}\.\d{1,2}\))$""", business)
+    Log f date
+    Log f str1
+    val sectors = allMatched("""(【.*?】[^【]*).*""", str1)
+    Log f sectors
+    
+    
+    val reData = """(.*?)(\d+)(\(\d+\))?""".r
+    for(sector <- sectors){
+      val (header, str2) = matchedAndRest("""(【.*?】).*""", sector)
+      Log f header
+      Log f str2
+
+      val rows = {
+        def stringToPairs(raw: String): Tuple2[String, String] =
+          raw match{
+            case reData(title, rate, plofit) =>
+              if(plofit == null)
+                title -> rate
+              else
+                (title + plofit) -> rate
+          }
+
+        str2
+          .split('、').map(stringToPairs)
+          .map(p => s"""['${p._1}', ${p._2}]""")
+          .mkString(",\n")
+      }
+      Log f rows
+    }
+    
+    
+    
+    
+    val reBusiness = """(【.*?】)(.*?)(\(\d{4}\.\d{1,2}\))""".r
+    
     val reDate = """\(\d{4}\.\d{1,2}\)"""
     val reHeader = "【.*?】"
     val reOther = "【.*"
-    val reData = """(.*?)(\d+)(\(\d+\))?""".r
+
     
-    val business = data.getOrElse("事業", "")
-    
+    val reRestDate = """(.*?)(\(\d{4}\.\d{1,2}\))""".r
+    val (rest1, date2) = business match{
+      case reRestDate(rest, date) => (rest, date)
+    }
+    val reHeaderRest = """(【.*?】)(.*)""".r
+    val (header2, rest2) = rest1 match{
+      case reHeaderRest(header, rest) => (header, rest)
+    }
+
+    /*
     val date =
       reDate.r.findFirstMatchIn(business) match {
         case Some(m) => m.group(0)
@@ -54,5 +124,7 @@ object PageMakerJp {
     
     val html = tEngine(data ++ added)
     Text.write("data/rss/%s.html".format(data("ID")), html)
+    * 
+    */
   }
 }
