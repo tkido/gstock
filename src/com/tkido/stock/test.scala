06122a41b29c527e7a43717233766b20361700bd
tkido <takanorikido@gmail.com>
2015-10-05 22:49:52 +0900

    Add "ProfitAttributableToOwnersOfParent"

--- src/com/tkido/stock/test.scala
+++ src/com/tkido/stock/test.scala
@@ -8,12 +8,14 @@ import com.tkido.tools.Tengine
 object test extends App {
   Log.level = Config.logLevel
   
-  val te = Tengine("data/rss/test.html")
-  val text = te(Map("title" -> "テストだよ！"))
-  Log d text
+  Log d com.tkido.stock.tdnet.XbrlParser("C:\\OLS\\xbrl\\tdnet\\4834\\tse-qcedjpsm-48340-20150924402095-ixbrl.htm")
   
-  Log d Ranking("2121")
-  Log d Spider("3085")
+  //val te = Tengine("data/rss/test.html")
+  //val text = te(Map("title" -> "テストだよ！"))
+  //Log d text
+  
+  //Log d Ranking("2121")
+  //Log d Spider("3085")
   
   Log.close()
 }
