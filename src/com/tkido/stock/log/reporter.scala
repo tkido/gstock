250657072ce70332ec6d432b35c3227ed9585a57
tkido <takanorikido@gmail.com>
2015-10-06 01:03:02 +0900

    Refactoring

--- src/com/tkido/stock/log/reporter.scala
+++ src/com/tkido/stock/log/reporter.scala
@@ -5,12 +5,13 @@ import com.tkido.tools.Log
 
 object Reporter {
   Log d "log.Reporter parse rireki.txt"
-  val data = Parser("data/log/rireki.txt")
+  val dataMap = Parser("data/log/rireki.txt")
   
   def apply(code:String) :String = {
-    val header = Html.toTrTh("日付", "取引", "数量", "価格", "金額")
-    val list = data(code).map(Html.toTrTd(_: _*))
+    if(!dataMap.contains(code)) return ""
     
+    val header = Html.toTrTh("日付", "取引", "数量", "価格", "金額")
+    val list = dataMap(code).map(Html.toTrTd(_: _*))
     """<h3>売買ログ</h3>""" +
     Html.toTable(header :: list.reverse)
   }
