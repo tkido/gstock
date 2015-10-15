f05dad98fa70e4d1e30cf0c5b0439a10983edcf0
tkido <takanorikido@gmail.com>
2015-10-15 17:56:26 +0900

    Refactoring Search

--- src/com/tkido/stock/spider/spiderUsSummary.scala
+++ src/com/tkido/stock/spider/spiderUsSummary.scala
@@ -2,57 +2,30 @@ package com.tkido.stock.spider
 
 import com.tkido.tools.Html
 import com.tkido.tools.Log
-import com.tkido.tools.tryOrElse
+import com.tkido.tools.Search
 
 object SpiderUsSummary {
   def apply(code:String) :Map[String, String] = {
     Log d s"SpiderUsSummary Spidering ${code}"
     
-    def get :Map[String, String] = {
-      val html = Html("http://finance.yahoo.com/q?s=%s".format(code))
-  
-      val name =
-        html.getGroupOf("""^.*?<div class="title"><h2>(.*?) \(.*?\)</h2>.*$""".r).replaceAll("&amp;", "&")
-      val market =
-        html.getGroupOf("""^.*?<span class="rtq_exch"><span class="rtq_dash">-</span>(.*?)  </span>.*$""".r)
-      val price =
-        html.getGroupOf("""^.*?<span class="time_rtq_ticker"><span id="yfs_l84_.*?">(.*?)</span></span>.*$""".r)
-      val ratio = {
-        val raw = html.getGroupOf("""^.*?<div class="title">.*?class=".*?_arrow" alt=".*?">   .*?</span><span id="yfs_p43_.*?">\((.*?)%\)</span>.*$""".r)
-        val sign = html.getGroupOf("""^.*?<div class="title">.*?class=".*?_arrow" alt="(.*?)".*$""".r) match{
-          case "Up"   => ""
-          case "Down" => "-"
-          case _      => ""
-        }
-        sign + raw
-      }
-      val volume = {
-        val raw = html.getGroupOf("""^.*?Volume:</th><td class="yfnc_tabledata1"><span id="yfs_v53_.*?">(.*?)</span>.*$""".r)
-        "=" + raw.replaceAll(",", "") + "/【発行】"
-      }
-      val lastClose =
-        html.getGroupOf("""^.*?Prev Close:</th><td class="yfnc_tabledata1">(.*?)</td>.*$""".r)
-      val bid =
-        html.getGroupOf("""^.*?Bid:</th><td class="yfnc_tabledata1"><span id="yfs_b00_.*?">(.*?)</span>.*$""".r)
-      val bidNum =
-        html.getGroupOf("""^.*?Bid:</th><td class="yfnc_tabledata1"><span id="yfs_b00_.*?">.*?</span><small> x <span id="yfs_b60_.*?">(.*?)</span>.*$""".r)
-      val ask =
-        html.getGroupOf("""^.*?Ask:</th><td class="yfnc_tabledata1"><span id="yfs_a00_.*?">(.*?)</span>.*$""".r)
-      val askNum =
-        html.getGroupOf("""^.*?Ask:</th><td class="yfnc_tabledata1"><span id="yfs_a00_.*?">.*?</span><small> x <span id="yfs_a50_.*?">(.*?)</span>.*$""".r)
-      
-      Map("名称"   -> name,
-          "市"     -> market,
-          "現値"   -> price,
-          "前比"   -> ratio,
-          "出来"   -> volume,
-          "前終"   -> lastClose,
-          "最売"   -> ask,
-          "最売数" -> askNum,
-          "最買"   -> bid,
-          "最買数" -> bidNum )
-    }
-    
-    tryOrElse(get _, Map())
+    val html = Html("http://finance.yahoo.com/q?s=%s".format(code))
+    val map = html.search(List(
+      Search("名称", """^.*?<div class="title"><h2>(.*?) \(.*?\)</h2>.*$""".r, Search.GROUP, _.replaceAll("&amp;", "&")),
+      Search("市", """^.*?<span class="rtq_exch"><span class="rtq_dash">-</span>(.*?)  </span>.*$""".r, Search.GROUP, s => s),
+      Search("現値", """^.*?<span class="time_rtq_ticker"><span id="yfs_l84_.*?">(.*?)</span></span>.*$""".r, Search.GROUP, s => s),
+      Search("出来", """^.*?Volume:</th><td class="yfnc_tabledata1"><span id="yfs_v53_.*?">(.*?)</span>.*$""".r, Search.GROUP, raw => "=%s/【発行】".format(raw.replaceAll(",", ""))),
+      Search("前終", """^.*?Prev Close:</th><td class="yfnc_tabledata1">(.*?)</td>.*$""".r, Search.GROUP, s => s),
+      Search("最売", """^.*?Ask:</th><td class="yfnc_tabledata1"><span id="yfs_a00_.*?">(.*?)</span>.*$""".r, Search.GROUP, s => s),
+      Search("最買", """^.*?Bid:</th><td class="yfnc_tabledata1"><span id="yfs_b00_.*?">(.*?)</span>.*$""".r, Search.GROUP, s => s),
+      Search("最売数", """^.*?Ask:</th><td class="yfnc_tabledata1"><span id="yfs_a00_.*?">.*?</span><small> x <span id="yfs_a50_.*?">(.*?)</span>.*$""".r, Search.GROUP, s => s),
+      Search("最買数", """^.*?Bid:</th><td class="yfnc_tabledata1"><span id="yfs_b00_.*?">.*?</span><small> x <span id="yfs_b60_.*?">(.*?)</span>.*$""".r, Search.GROUP, s => s),
+      Search("raw", """^.*?<div class="title">.*?class=".*?_arrow" alt=".*?">   .*?</span><span id="yfs_p43_.*?">\((.*?)%\)</span>.*$""".r, Search.GROUP, s => s),
+      Search("sign", """^.*?<div class="title">.*?class=".*?_arrow" alt="(.*?)".*$""".r, Search.GROUP, s => s match{
+        case "Up"   => ""
+        case "Down" => "-"
+        case _      => ""
+      }))
+    )
+    map ++ Map("前比" -> (map("sign") + map("raw")))
   }
 }
