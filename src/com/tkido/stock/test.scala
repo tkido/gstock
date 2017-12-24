305cf7b54d5461ee8263665f0af63f6c633ac22c
tkido <takanorikido@gmail.com>
2017-12-25 06:10:42 +0900

    cache filename

--- src/com/tkido/stock/test.scala
+++ src/com/tkido/stock/test.scala
@@ -1,17 +1,20 @@
 package com.tkido.stock
 
-import com.tkido.stock.spider.SpiderJpConsolidate
-import com.tkido.stock.spider.SpiderJpProfile
-import com.tkido.tools.Log
+import java.io.File
+import java.security.MessageDigest
 
-import com.tkido.tools.Html
-import com.tkido.tools.Search
+import com.tkido.stock.edinet.XbrlFinder
+import com.tkido.tools.Log
 
 object test extends App {
   Log.logging(Config.logLevel, main)
   
   def main() {
-    Log d SpiderJpConsolidate("3085")
-    Log d SpiderJpProfile("3085")
+    val md = java.security.MessageDigest.getInstance("SHA-1")
+    
+    val files = XbrlFinder("2685")
+    val names = files.map(new File(_).getName)
+    md.update(names.mkString.getBytes)
+    Log f md.digest().map("%02x".format(_)).mkString
   }
 }
